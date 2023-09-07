85da2ade9ef633db056b2b027afd0d6bac8c4c7d
abbeza <bezawitabel.tria.com>
2023-09-07 01:43:12 -0700

    first c programing

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* main - prints sentences
+* Return: returns zero
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* main - prints sentences using printf
+* Return: returns zero
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
